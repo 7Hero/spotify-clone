2184155984e6386964306be375275680807fc320
7Hero <vpoopescu07@gmail.com>
2021-12-29 18:55:50 +0200

    vasea

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,12 +1,17 @@
-import { getToken } from "next-auth/jwt";
+import { decode, getToken } from "next-auth/jwt";
+import { getSession } from "next-auth/react"
 import { NextResponse } from "next/server";
 
 async function middleware(req,res){
-
-  const token = await getToken({ req, secret: 'vasea' });
-
+  // const token = await getSession( { req });
+  //next-auth.session-token
+  const { cookies } = req;
+  const token = cookies["next-auth.session-token"];
+  // console.log(cookies["next-auth.session-token"])
+  // const token = await getToken({ req, secret: 'vasea' });
+  const decryptedtoken = await decode({ token, secret: 'vasea' })
+  console.log(decryptedtoken?.name);
   const { pathname } = req.nextUrl;
-  console.log(token,pathname)
   if( pathname === '/login' && token) return NextResponse.redirect("/");
 
   if( token || pathname.includes('/api/auth')) return NextResponse.next()
