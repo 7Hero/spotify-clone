52cb4694aff35ebcd1c2b61a6df44fb4038b87ca
7Hero <vpoopescu07@gmail.com>
2021-12-29 17:41:10 +0200

    s

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,7 +6,7 @@ async function middleware(req,res){
   const token = await getToken({ req,secret: process.env.JWT_SECRET });
 
   const pathname = req.url;
-  console.log(token)
+  console.log(token,pathname)
   if( pathname === '/login' && token) return NextResponse.redirect("/");
 
   if( token || pathname.includes('/api/auth')) return NextResponse.next()
