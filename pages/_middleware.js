72fff54f4904684bd7acd01cff5fd68036d62b99
7Hero <vpoopescu07@gmail.com>
2021-12-29 17:27:47 +0200

    vasea

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,7 +6,7 @@ async function middleware(req,res){
   const token = await getToken({ req,secret: process.env.JWT_SECRET });
 
   const pathname = req.url;
-  
+  console.log(token)
   if( pathname === '/login' && token) return NextResponse.redirect("/");
 
   if( token || pathname.includes('/api/auth')) return NextResponse.next()
