b5ec564da6b1f99484db1816707ae7014422b2ff
7Hero <vpoopescu07@gmail.com>
2021-12-29 18:11:07 +0200

    vasea

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,7 @@ function Login() {
     <div className='w-screen h-screen flex justify-center items-center'>
       <button
       onAnimationEnd={(e) => { e.target.blur()}}
-      onClick={() => signIn("spotify", { callbackUrl: "/" })}
+      onClick={() => signIn()}
       className='bg-green-500 py-2 px-3 rounded-xl focus:animate-click shadow-lg transition-all duration-150'>
         <p className="text-white font-sans font-semibold">
           Login with Spotify
